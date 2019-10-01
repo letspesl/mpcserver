55a48abe247f3b8988654d535192bedf1775d53e
pesl0523 <pesl0523@pesl0523ui-iMac.local>
2019-10-01 15:51:48 +0900

    add peer apis

--- libs/api/protocol.js
+++ libs/api/protocol.js
@@ -14,10 +14,14 @@ const Party = mongo.Party;
 const Round = mongo.Round;
 
 async function getHosts(parties) {
-    return await Promise.map(parties, async function(partyId) {
+    let hosts = [];
+    await Promise.map(parties, async function(partyId) {
         const party = await Party.findOne({_id: partyId});
-        return party.host;
+        if(!_.isEqual(party.host, "device_id")) {
+            hosts.push(party.host);
+        }
     });
+    return hosts;
 }
 
 async function createProtocol(reqBody) {
@@ -26,12 +30,15 @@ async function createProtocol(reqBody) {
     const shareCount = reqBody.share_count;
     const threshold = reqBody.threshold;
 
-    const parties = config.party;
+    let parties = JSON.parse(JSON.stringify(config.party)); 
     if(reqBody.hasOwnProperty("parties")) {
         parties = reqBody.parties;
     }
     
     parties.unshift(partyId);
+    if(parties.length > shareCount) {
+        parties = parties.slice(0, shareCount);
+    }
     
     let protocol = await Protocol.create({
         protocol_type: protocolType,
@@ -41,20 +48,18 @@ async function createProtocol(reqBody) {
     });
 
     const hosts = await getHosts(parties);
-    
+
     await Promise.each(hosts, async function(host) {
-        if(!_.isEqual(host, "device_id")) {
-            const body = _.pick(protocol, ['parties_count', 'protocol_type', 'share_count', 'threshold']);
-            await Nblocks.inviteParty(host, protocol._id, body);
-            await Protocol.updateOne({_id : protocol._id}, {$inc : {parties_count : 1}});
-            protocol = await Protocol.findOne({_id: protocol._id});
-        }  
+        const body = _.pick(protocol, ['parties_count', 'protocol_type', 'share_count', 'threshold']);
+        await Nblocks.inviteParty(host, protocol._id, body);
+        await Protocol.updateOne({_id : protocol._id}, {$inc : {parties_count : 1}});
+        protocol = await Protocol.findOne({_id: protocol._id});
     })
     .catch((err) => {
         throw SErr.create('BAD_REQUEST', {hosts : hosts, protocol : protocol, reason : 'createProtocol fail', error : err});
     });
 
-    return protocol;
+    return {id: protocol._id, party_idx: 1};
 }
 
 async function sendBroadcast(protocolId, reqBody) {
@@ -64,9 +69,6 @@ async function sendBroadcast(protocolId, reqBody) {
     const partyIdx = reqBody.party_idx;
     const round = reqBody.round;
     const value = reqBody.value;
-
-    console.log(typeof partyIdx);
-    console.log(typeof round);
 
     await Promise.map(protocol.parties, async function(party) {      
         if(!_.isEqual(party, partyId)) {
@@ -91,19 +93,56 @@ async function pollBroadcast(protocolId, reqBody) {
 
     const rounds = await Round.find({protocol_id: protocolId, step: step, to: partyId});
 
-    let values = rounds.map((round) => {
-        return _.pick(round, ['from_idx', 'data']);
+    let values = [];
+    
+    rounds.map((round) => {
+        values.push(_.pick(round, ['from_idx', 'data']));
+        // 통신 데이터 제거 > redis로 변경 예정
+        // Round.deleteOne({_id: round._id})
+        // .then((res) => {
+        //     console.log(res);
+        // });
     })
 
     return {size: rounds.length, rounds: JSON.stringify(values)};
 }
 
-async function sendPeer(reqBody) {
+async function sendPeer(protocolId, reqBody) {
+    const protocol = await Protocol.findOne({_id: protocolId});
+    
+    const partyIdx = reqBody.party_idx;
+    const round = reqBody.round;
+    const value = reqBody.value;
+    const toIdx = parseInt(reqBody.to_idx) - 1;
+    if(toIdx < 0 || toIdx >= protocol.parties.length) {
+        throw SErr.create('BAD_REQUEST', {toIdx : toIdx, reason : 'sendPeer fail', error : err});
+    }
+    const to = protocol.parties[toIdx];
 
+    await Round.create({
+        protocol_id : protocol._id,
+        step : round,
+        data : value,
+        from_idx : partyIdx,
+        to : to,
+    }).catch((err) => {
+        throw SErr.create('BAD_REQUEST', {protocol : protocol, reason : 'sendPeer fail', error : err});
+    });   
+
+    return {protocol_id: protocol._id, round: round};
 }
 
-async function pollPeer(reqBody) {
-    
+async function pollPeer(protocolId, reqBody) {
+    const partyId = reqBody.party_id;
+    const step = reqBody.round;
+
+    const rounds = await Round.find({protocol_id: protocolId, step: step, to: partyId});
+
+    let values = rounds.map((round) => {
+        return _.pick(round, ['from_idx', 'data']);
+    })
+
+    return {size: rounds.length, rounds: JSON.stringify(values)};
 }
 
 module.exports = exports = {createProtocol, sendBroadcast, pollBroadcast, sendPeer, pollPeer};
--- routes/api/protocol.js
+++ routes/api/protocol.js
@@ -9,7 +9,7 @@ const router = express.Router();
 const createProtocol = function(req, res, next) {
     Protocol.createProtocol(req.body)
         .then((result) => {
-            res.json({id: result._id, party_idx: 1});
+            res.json(result);
         })
         .catch((err) => {
             SErr.sendRes(res, err);
@@ -17,55 +17,45 @@ const createProtocol = function(req, res, next) {
 };
 
 const sendBroadcast = function(req, res, next) {
-    console.log('routes sendBroadcast req.body', req.body);
     const protocolId = req.params.protocol_id;
     Protocol.sendBroadcast(protocolId, req.body)
         .then((result) => {
-            console.log('routes sendBroadcast result', result);
             res.json(result);
         })
         .catch((err) => {
-            console.log('routes sendBroadcast err', err);
             SErr.sendRes(res, err);
         });
 };
 
 const pollBroadcast = function(req, res, next) {
-    console.log('routes pollBroadcast req.body', req.body);
     const protocolId = req.params.protocol_id;
     Protocol.pollBroadcast(protocolId, req.body)
         .then((result) => {
-            console.log('routes pollBroadcast result', result);
             res.json(result);
         })
         .catch((err) => {
-            console.log('routes pollBroadcast err', err);
             SErr.sendRes(res, err);
         });
 };
 
 const sendPeer = function(req, res, next) {
-    console.log('routes sendPeer req.body', req.body);
-    Protocol.sendPeer(req.body)
+    const protocolId = req.params.protocol_id;
+    Protocol.sendPeer(protocolId, req.body)
         .then((result) => {
-            console.log('routes sendPeer result', result);
             res.json(result);
         })
         .catch((err) => {
-            console.log('routes sendPeer err', err);
             SErr.sendRes(res, err);
         });
 };
 
 const pollPeer = function(req, res, next) {
-    console.log('routes pollPeer req.body', req.body);
-    Protocol.pollPeer(req.body)
+    const protocolId = req.params.protocol_id;
+    Protocol.pollPeer(protocolId, req.body)
         .then((result) => {
-            console.log('routes pollPeer result', result);
             res.json(result);
         })
         .catch((err) => {
-            console.log('routes pollPeer err', err);
             SErr.sendRes(res, err);
         });
 };
@@ -76,7 +66,7 @@ router.post('/1/protocols', createProtocol);
 router.post('/1/protocols/broadcast/send/:protocol_id', sendBroadcast);
 router.post('/1/protocols/broadcast/poll/:protocol_id', pollBroadcast);
 
-router.post('/1/protocols/peer/:protocol_id', sendPeer);
-router.get('/1/protocols/peer/:protocol_id', pollPeer);
+router.post('/1/protocols/peer/send/:protocol_id', sendPeer);
+router.post('/1/protocols/peer/poll/:protocol_id', pollPeer);
 
 module.exports = exports = router;
